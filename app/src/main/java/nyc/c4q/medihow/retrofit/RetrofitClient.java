d32fbdbcdc99bf89987c525519766a71d8e504bb
Yoki Lam <yokeyenlam@ac.c4q.nyc>
2018-03-03 12:40:20 -0500

    working on the retrofit

---
+++ app/src/main/java/nyc/c4q/medihow/retrofit/RetrofitClient.java
@@ -0,0 +1,24 @@
+package nyc.c4q.medihow.retrofit;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by yokilam on 3/3/18.
+ */
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getRetrofit(String basUrl) {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(basUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
