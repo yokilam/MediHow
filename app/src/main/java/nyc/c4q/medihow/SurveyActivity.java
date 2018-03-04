5d2530ce41e6c3344e8a2ce9634444777fb0dc20
Kevin Toro <kevintoro@ac.c4q.nyc>
2018-03-04 00:17:55 -0500

    survey getter method

--- app/src/main/java/nyc/c4q/medihow/SurveyActivity.java
+++ app/src/main/java/nyc/c4q/medihow/SurveyActivity.java
@@ -18,14 +18,25 @@ public class SurveyActivity extends AppCompatActivity {
    @Override
     public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
+       //setContentView(R.id.);
+
        getSurveyQuestions();
    }
-    SurveyQuestions surveyQuestions= new SurveyQuestions();
+
     public SurveyQuestions getSurveyQuestions() {
+        SurveyQuestions surveyQuestions= new SurveyQuestions();
         surveyQuestions.questions[0] = getString(R.string.prove_identity);
+        surveyQuestions.questions[1] = getString(R.string.prove_income);
+        surveyQuestions.questions[2] = getString(R.string.prove_address);
+        surveyQuestions.questions[3] = getString(R.string.prove_dependent_care);
+        surveyQuestions.questions[4] = getString(R.string.prove_previous_insurance);
+        surveyQuestions.questions[5] = getString(R.string.prove_pregnancy);
+        surveyQuestions.questions[6] = getString(R.string.prove_immigrant_status);
+
         Log.d(RETRIEVE, "getSurveyQuestions: ");
         return surveyQuestions;
     }
+
 
 
 }
