456ff74a71dbc264a5cb788a7f5ceeb30faed93f
Jervon Arnold <jervonjarnold@gmail.com>
2018-03-05 13:31:33 -0500

    added splash screen bug

--- app/src/main/java/nyc/c4q/medihow/activites/SplashActivity.java
+++ app/src/main/java/nyc/c4q/medihow/activites/SplashActivity.java
@@ -20,8 +20,9 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 Intent intent= new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
-
+                finish();
             }
         },3000);
+
     }
 }
